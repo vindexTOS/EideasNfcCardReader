593e3d0ddad4dc744b408beda7e5b7bf65683668
vindexTOS <giorgikutateladze1998@gmail.com>
2024-11-26 22:34:45 +0400

    removed depricated invetnories

--- components/rfcReader.tsx
+++ components/rfcReader.tsx
@@ -1,50 +1,50 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
-import React, { useState, useEffect } from "react";
+// import { Image, StyleSheet, Platform, View, Text } from "react-native";
+// import React, { useState, useEffect } from "react";
  
-import nfcManager , {NfcEvents}from "react-native-nfc-manager";
+// import nfcManager , {NfcEvents}from "react-native-nfc-manager";
 
-export default function HomeScreen() {
-  const [hasNfc, setHasNfc] = useState<boolean | null>(null);
-  const [cardInfo,setCardInfo] = useState('')
-   async function scanTag(){
-     nfcManager.setEventListener(NfcEvents.DiscoverTag,(tag:any )=> {
-       console.log(tag)
-       setCardInfo(tag)
-     })
+// export default function HomeScreen() {
+//   const [hasNfc, setHasNfc] = useState<boolean | null>(null);
+//   const [cardInfo,setCardInfo] = useState('')
+//    async function scanTag(){
+//      nfcManager.setEventListener(NfcEvents.DiscoverTag,(tag:any )=> {
+//        console.log(tag)
+//        setCardInfo(tag)
+//      })
 
-    await  nfcManager.registerTagEvent()
-   }
-  async function checkNf() {
-    const isSupported = await nfcManager.isSupported();
-     if(isSupported){
-       await nfcManager.start()
-     }
-    setHasNfc(isSupported);
-  }
-  useEffect(() => {
-    checkNf();
-  }, []);
+//     await  nfcManager.registerTagEvent()
+//    }
+//   async function checkNf() {
+//     const isSupported = await nfcManager.isSupported();
+//      if(isSupported){
+//        await nfcManager.start()
+//      }
+//     setHasNfc(isSupported);
+//   }
+//   useEffect(() => {
+//     checkNf();
+//   }, []);
 
-  if (hasNfc) {
-    return (
-      <View>
-        <Text>Your Device Supports NFC </Text>
-        <Text>Your Device Supports NFC </Text>
-        <Text>Your Device Supports NFC </Text>
-        <Text onPress={scanTag} >START</Text>
-        <Text>{JSON.stringify(cardInfo)}</Text>
-      </View>
-    );
-  } else {
-    return (
-      <View>
-        <Text>Your Device DOSE NOT! Supports NFC </Text>
-        <Text>Your Device DOSE NOT! Supports NFC </Text>
+//   if (hasNfc) {
+//     return (
+//       <View>
+//         <Text>Your Device Supports NFC </Text>
+//         <Text>Your Device Supports NFC </Text>
+//         <Text>Your Device Supports NFC </Text>
+//         <Text onPress={scanTag} >START</Text>
+//         <Text>{JSON.stringify(cardInfo)}</Text>
+//       </View>
+//     );
+//   } else {
+//     return (
+//       <View>
+//         <Text>Your Device DOSE NOT! Supports NFC </Text>
+//         <Text>Your Device DOSE NOT! Supports NFC </Text>
 
-        <Text>Your Device DOSE NOT! Supports NFC </Text>
+//         <Text>Your Device DOSE NOT! Supports NFC </Text>
 
-        <Text>Your Device DOSE NOT! Supports NFC </Text>
-      </View>
-    );
-  }
-}
+//         <Text>Your Device DOSE NOT! Supports NFC </Text>
+//       </View>
+//     );
+//   }
+// }
